1289281e3b6cc9528a8440ce5e094bcf29ec8375
Dusty211 <kylebenz@hotmail.com>
2019-04-08 10:12:50 -0400

    refactor for better o

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -11,23 +11,54 @@ class RecurringMoment
 
   def match(date)
 
-    new_date = @start
-    next_interval = @interval
-
-    while new_date <= date
-      if new_date == date
+    if @period == 'monthly'
+      # binding.pry
+      months_diff = (date.year * 12 + date.month) - (@start.year * 12 + @start.month)
+      if (date.day != @start.day) && (@start.advance(months: months_diff).day != date.day)
+        return false
+      elsif months_diff % @interval == 0
         return true
+      else
+        return false
       end
 
-      if @period == 'monthly'
-        new_date = @start.advance(months: next_interval)
-      elsif @period == 'weekly'
-        new_date = @start.advance(weeks: next_interval)
-      elsif @period == 'daily'
-        new_date = @start.advance(days: next_interval)
+    elsif @period == 'weekly'
+      weeks_diff = (date - @start) / 7
+      if weeks_diff % @interval == 0
+        return true
+      else
+        return false
       end
-      next_interval += @interval
-    end
+
+    elsif @period == 'daily'
+      days_diff = date - @start
+      if days_diff % @interval == 0
+        return true
+      else
+        return false
+      end
+end
+
+    #
+    # binding.pry
+    #
+    # new_date = @start
+    # next_interval = @interval
+    #
+    # while new_date <= date
+    #   if new_date == date
+    #     return true
+    #   end
+    #
+    #   if @period == 'monthly'
+    #     new_date = @start.advance(months: next_interval)
+    #   elsif @period == 'weekly'
+    #     new_date = @start.advance(weeks: next_interval)
+    #   elsif @period == 'daily'
+    #     new_date = @start.advance(days: next_interval)
+    #   end
+    #   next_interval += @interval
+    # end
 
     return false
   end
